b9b9198b1f869fc022c0f11b119e5f15dce3acc8
Thushara Dahanayake <thushara.d@interblocks.com>
2021-03-09 11:24:45 +0530

    add firebase to prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBXv6uihSWpPvjGatzXg5Jcaf6zpwVn7dE",
+    authDomain: "stockupdater-4089c.firebaseapp.com",
+    databaseURL: "https://stockupdater-4089c-default-rtdb.firebaseio.com",
+    projectId: "stockupdater-4089c",
+    storageBucket: "stockupdater-4089c.appspot.com",
+    messagingSenderId: "1005629676390",
+    appId: "1:1005629676390:web:00aa831b67a048058ec6a7",
+    measurementId: "G-M5MKFXVXHQ"
+  }
 };
