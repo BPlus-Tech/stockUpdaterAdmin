35edc824ceec5faba3c18d34606f3a4e8ad6f28a
Thushara Dahanayake <thushara.d@interblocks.com>
2021-03-09 10:13:44 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
+import { AppComponent } from './app.component';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../environments/environment";
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    NgbModule,
+    ToastrModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+
+  ],
+  providers: [
+      AngularFirestore
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBXv6uihSWpPvjGatzXg5Jcaf6zpwVn7dE",
+    authDomain: "stockupdater-4089c.firebaseapp.com",
+    databaseURL: "https://stockupdater-4089c-default-rtdb.firebaseio.com",
+    projectId: "stockupdater-4089c",
+    storageBucket: "stockupdater-4089c.appspot.com",
+    messagingSenderId: "1005629676390",
+    appId: "1:1005629676390:web:00aa831b67a048058ec6a7",
+    measurementId: "G-M5MKFXVXHQ"
+  }
+};
